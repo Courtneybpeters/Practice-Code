935131bbf437d42d4f149a4d1e4245edc322b799
Courtneybpeters <courtneybpeters@gmail.com>
2014-05-13 23:02:01 -0500

    Hello World in C#
    First program in C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Hello = "Hello World";
+            Console.WriteLine(Hello);
+            Console.ReadKey(true);
+        }
+    }
+}
