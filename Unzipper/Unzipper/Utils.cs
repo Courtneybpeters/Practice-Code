b27993117be731489b5a5227233de52f2badf955
Luke Paireepinart <lukepdev@live.com>
2014-05-19 16:03:55 -0500

    SharpCompress example
    added sharpcompress example and also added some sample zip files.

---
+++ Unzipper/Unzipper/Utils.cs
@@ -0,0 +1,25 @@
+﻿using SharpCompress.Archive;
+using SharpCompress.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unzipper
+{
+    class Utils
+    {
+        public static void Extract(string archivePath, string destPath)
+        {
+            var archive = ArchiveFactory.Open(archivePath);
+            foreach (var entry in archive.Entries)
+            {
+                if (!entry.IsDirectory)
+                {
+                    entry.WriteToDirectory(destPath, ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                }
+            }
+        }
+    }
+}
